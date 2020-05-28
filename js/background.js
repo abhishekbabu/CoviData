85fcf1a73a8119d9e43bd41a031e01a6ccaad8a7
Abhishek Babu <abhideep0408@gmail.com>
2020-05-27 18:31:05 -0700

    Added functions to get country/state list and data for specific country/state

--- js/background.js
+++ js/background.js
@@ -1,12 +1,51 @@
-const testUrl = "https://disease.sh/v2/nyt/usa";
+const COUNTRIES_URL = "https://api.covid19api.com/countries";
+const COUNTRY_DATA_URL = "https://api.covid19api.com/total/country";
+const USA_STATES_URL = "https://disease.sh/v2/historical/usacounties";
 
 chrome.runtime.onInstalled.addListener(() => {
   console.log("installed");
-  testRequest();
+  GetCountryList();
+  GetCountryData("united-states")
+  GetUSAStateList();
+  GetUSAStateData("washington");
 });
 
-async function testRequest() {
-  const response = await fetch(testUrl);
-  const data = await response.json();
-  console.log(data);
+async function GetCountryList() {
+  try {
+    let response = await fetch(COUNTRIES_URL);
+    let parsed = await response.json();
+    console.log(parsed)
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+async function GetCountryData(country) {
+  try {
+    let response = await fetch(`${COUNTRY_DATA_URL}/` + country);
+    let parsed = await response.json();
+    console.log(parsed)
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+async function GetUSAStateList() {
+  try {
+    let response = await fetch(USA_STATES_URL);
+    let parsed = await response.json();
+    console.log(parsed)
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+async function GetUSAStateData(state) {
+  try {
+    let response = await fetch(`${USA_STATES_URL}/` + state);
+    let parsed = await response.json();
+    console.log(parsed)
+  } catch (err) {
+    console.log(err.message)
+  }
 }
